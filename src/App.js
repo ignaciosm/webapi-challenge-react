25cc21bd03e2289f25318f3ca480235dba3af5cb
ignaciosm <ignaciosm@gmail.com>
2019-12-06 13:27:22 -0500

    frontend working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://cors-anywhere.herokuapp.com/https://webapi-back.herokuapp.com/projects')
+    .then(res => {
+      setProjects(res.data)
+    })
+    .catch(err => { console.log('error')})
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {projects.map(project => (
+        <>
+        <h1>{`Project Name: ${project.name}`}</h1>
+        <p>{project.description}</p>
+        </>
+      ))}
     </div>
   );
 }
